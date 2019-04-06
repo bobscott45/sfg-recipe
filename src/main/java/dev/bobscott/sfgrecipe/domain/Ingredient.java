7d15c7d2b722e53547f9a37ad5fd8743cdc29fe6
Bob <bob@rwscott.co.uk>
2019-04-06 10:36:06 +0100

    Add domain entities: Ingredient, Notes, Recipe, UnitOfMeasure

---
+++ src/main/java/dev/bobscott/sfgrecipe/domain/Ingredient.java
@@ -0,0 +1,61 @@
+package dev.bobscott.sfgrecipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)  //EAGER is the default
+    private UnitOfMeasure unitOfMeasure;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return unitOfMeasure;
+    }
+
+    public void setUom(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
---
+++ src/main/java/dev/bobscott/sfgrecipe/domain/Notes.java
@@ -0,0 +1,44 @@
+package dev.bobscott.sfgrecipe.domain;
+
+import javassist.bytecode.ByteArray;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNotes() {
+        return recipeNotes;
+    }
+
+    public void setNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
